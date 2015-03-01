69759fb43d2aa9cd2fea36bcaacaf3e10a2f006c
szkodnik25 <szkodnik25@users.noreply.github.com>
2015-03-01 20:51:18 +0100

    Update RestClient.java

--- Android/showcases/SerwerTest/src/com/example/restclient/RestClient.java
+++ Android/showcases/SerwerTest/src/com/example/restclient/RestClient.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 public interface RestClient<Type> {
 	
 	public Collection<Type> 	GET();
-	public void 				DELETE();
-	public void 				POST(Type object) throws IOException;
-	public void 				PUT(Type object);
+	public void 			DELETE();
+	public void 			POST(Type object) throws IOException;
+	public void 			PUT(Type object);
 }

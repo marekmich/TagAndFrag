b961bbe253c68b8dee067105e79cfefd2baccfb9
Łukasz Żurawski <LukasZurawski@gmail.com>
2015-04-11 10:44:32 +0200

    Dodanie kontenera

---
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/TagAndFragContainer.java
@@ -0,0 +1,16 @@
+package com.pz.tagandfrag;
+
+import java.util.Collection;
+
+import com.pz.tagandfrag.restclient.Game;
+import com.pz.tagandfrag.restclient.Player;
+import com.pz.tagandfrag.restclient.Team;
+
+public class TagAndFragContainer {
+
+	public static Game game;
+	public static Player player;
+	public static Collection<Team> teamList;
+	public static Collection<Player> players;
+	public static PreferencesManager preferences;
+}

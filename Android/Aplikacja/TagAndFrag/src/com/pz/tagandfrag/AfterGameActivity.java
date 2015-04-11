8f80471ed23b9896e2503dc2936e713f26dba3c9
Łukasz Żurawski <LukasZurawski@gmail.com>
2015-04-11 12:36:28 +0200

    Dodanie aktywności AfterGameActivity, która będzie 'sprzątać' po grze

---
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/AfterGameActivity.java
@@ -0,0 +1,15 @@
+package com.pz.tagandfrag;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AfterGameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_after_game);
+	}
+}

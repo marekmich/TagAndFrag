1fd933aa295303fbe2bd99841af4516bc81247a6
Łukasz Żurawski <LukasZurawski@gmail.com>
2015-04-11 13:47:58 +0200

    Usunięcie przycisku zakończenia aplikacji. Dodanie obsługi przycisku rozpoczęcia nowej gry. Drobne poprawki.

--- Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/AfterGameActivity.java
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/AfterGameActivity.java
@@ -1,7 +1,9 @@
 package com.pz.tagandfrag;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AfterGameActivity extends Activity {
 
@@ -19,7 +21,6 @@ public class AfterGameActivity extends Activity {
 		TagAndFragContainer.player.setTeam(0);
 		TagAndFragContainer.players.clear();
 		TagAndFragContainer.teamList.clear();
-		TagAndFragContainer.players.clear();
 		TagAndFragContainer.preferences.setTeam(0);
 		TagAndFragContainer.preferences.saveAllDataToPreferences();
 	}
@@ -27,7 +28,16 @@ public class AfterGameActivity extends Activity {
 	/////////////////////////////////
 	/* Zmiany w wygl¹dzie */
 	
+	//Dodaæ wyœwietlanie podsumowania gry
+	
 	/////////////////////////////////
 	/* Listenery */
-	
+	/**
+	 * Nas³uchuje ( ;) ) klikniêcia na przycisk odpowiedzialny za rozpoczêcie
+	 * kolejnej rozgrywki
+	 */
+	public void onResetGameButtonClicked(View view) {
+		Intent intent = new Intent(AfterGameActivity.this, ChooseTeamActivity.class);
+		startActivity(intent);
+	}
 }

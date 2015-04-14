db77bfd01f672a16ffc2ed13e232ee78981717b0
Luigi-pl <LukasZurawski@gmail.com>
2015-04-14 13:46:46 +0200

    Wyświetlanie listy graczy na aktywności StandbyActivity

--- Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/activity/StandbyActivity.java
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/activity/StandbyActivity.java
@@ -1,23 +1,40 @@
 package com.pz.tagandfrag.activity;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.pz.tagandfrag.R;
 import com.pz.tagandfrag.managers.DataManager;
+import com.pz.tagandfrag.managers.UpdateTeamTask;
+import com.pz.tagandfrag.restclient.Player;
 
 public class StandbyActivity extends Activity {
 
+	// Okres aktualizacji w milisekundach
+	public static final int UPDATE_PERIOD = 3000;
+	public static final int MINIMUM_NUMBER_OF_PLAYER = 1;
+	private Handler updateTeamHandler;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_standby);
+		updateTeamHandler = new Handler();
+		updateTeamHandler.removeCallbacks(updateTeamRunnable());
+		updateTeamHandler.postDelayed(updateTeamRunnable(), UPDATE_PERIOD);
 	}
 	
 	public void onStartGameButtonClicked(View view) {
@@ -25,7 +42,47 @@ public class StandbyActivity extends Activity {
 		task.execute();
 	}
 	
-
+	private void updateTableLayout(TableLayout teamLayout, Collection<Player> playerList, int color) {
+		int i = 0;
+		teamLayout.removeAllViews();
+		for(Player player : playerList) {
+			TableRow row = new TableRow(this);
+			row.setBackgroundColor(color);
+			TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+			row.setLayoutParams(lp);
+			TextView name = new TextView(this);
+			name.setText(player.getName());
+			row.addView(name);
+			teamLayout.addView(row, i);
+	        i++;
+		}
+	}
+	private void updateTeamList() {
+		TableLayout myTeam = (TableLayout) findViewById(R.id.table_my_team_standby_activity);
+		TableLayout oppositeTeam = (TableLayout) findViewById(R.id.table_opposite_team_standby_activity);
+		updateTableLayout(myTeam, DataManager.players, Color.GREEN);
+		updateTableLayout(oppositeTeam, DataManager.oppositePlayers, Color.RED);
+		
+		if(DataManager.players.size() >= MINIMUM_NUMBER_OF_PLAYER 
+				&& DataManager.oppositePlayers.size() >= MINIMUM_NUMBER_OF_PLAYER) {
+			findViewById(R.id.progress_bar_standby).setVisibility(ProgressBar.INVISIBLE);
+			findViewById(R.id.button_start_game).setVisibility(ProgressBar.VISIBLE);
+		}
+		Log.i("TEAM", DataManager.players.toString());
+	}
+	
+	private Runnable updateTeamRunnable() {
+		return new Runnable() {
+			
+			@Override
+			public void run() {
+				new UpdateTeamTask().execute();
+				updateTeamList();
+				updateTeamHandler.postDelayed(updateTeamRunnable(), UPDATE_PERIOD);
+			}
+		};
+	}
+	
 	private Runnable connectWithWeaponRunnable() {
 		return new Runnable() {
 			
@@ -68,6 +125,7 @@ public class StandbyActivity extends Activity {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			updateTeamHandler.removeCallbacks(updateTeamRunnable());
 			return null;
 		}
 	}

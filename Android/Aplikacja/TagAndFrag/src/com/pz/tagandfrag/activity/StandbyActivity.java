6dd3c157993063703551a1b9c25fe57343ee00c7
Mateusz Wrzos <mt.wrzos@gmail.com>
2015-04-14 11:57:46 +0200

    Łączenie z modułem bluetooth w broni.

--- Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/activity/StandbyActivity.java
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/activity/StandbyActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.pz.tagandfrag.R;
+import com.pz.tagandfrag.managers.DataManager;
 
 public class StandbyActivity extends Activity {
 
@@ -13,10 +14,26 @@ public class StandbyActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_standby);
+		connectWithWeaponTask();
 	}
 	
 	public void onStartGameButtonClicked(View view) {
 		Intent intent = new Intent(StandbyActivity.this, GameActivity.class);
 		startActivity(intent);
+	}
+	
+	private Runnable connectWithWeaponRunnable() {
+		return new Runnable() {
+			
+			@Override
+			public void run() {
+				String MAC = DataManager.preferences.getMAC();
+				DataManager.bluetoothService.connectWithDeviceByMacAddress(MAC);
+			}
+		};
+	}
+	
+	private void connectWithWeaponTask() {
+		new Thread(connectWithWeaponRunnable()).start();
 	}
 }

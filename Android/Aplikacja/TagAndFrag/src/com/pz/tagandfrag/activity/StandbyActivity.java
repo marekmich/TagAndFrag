837f22e65a1e48b8f40c58c372e9bb71a67ac090
Luigi-pl <LukasZurawski@gmail.com>
2015-04-14 11:26:22 +0200

    Stworzenie aktywności oczekującej na dołączenie graczy do drużyn

---
+++ Android/Aplikacja/TagAndFrag/src/com/pz/tagandfrag/activity/StandbyActivity.java
@@ -0,0 +1,18 @@
+package com.pz.tagandfrag.activity;
+
+import com.pz.tagandfrag.R;
+import com.pz.tagandfrag.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StandbyActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_standby);
+	}
+}
